a046398f29b1ec164acf5cee77ccea3a4fb6ab19
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-06-29 10:42:46 -0300

    refac(Inventory): method import_data to accept JSON files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,20 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
+    report = {'simples': SimpleReport, 'completo': CompleteReport}
+
     @classmethod
-    def import_data(cls, path, report_type):
+    def import_data(cls, path, type):
         with open(path, mode="r", encoding="utf-8") as file:
-            data = csv.DictReader(file, delimiter=",", quotechar='"')
-            list = [row for row in data]
+            if path.endswith(".csv"):
+                data = csv.DictReader(file, delimiter=",", quotechar='"')
+                list = [row for row in data]
+                return cls.report[type].generate(list)
 
-            if report_type == 'simples':
-                return SimpleReport.generate(list)
-
-            if report_type == 'completo':
-                return CompleteReport.generate(list)
+            elif path.endswith(".json"):
+                list = json.load(file)
+                return cls.report[type].generate(list)
