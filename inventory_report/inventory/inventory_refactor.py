ee9c31434095591585513b2ff89b146a67f04d91
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-07-01 11:09:22 -0300

    feat: implements class InventoryRefactor

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,19 @@
+from collections.abc import Iterable
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class InventoryRefactor(Iterable):
+    report = {'simples': SimpleReport, 'completo': CompleteReport}
+
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
+    def import_data(self, path, type):
+        self.data += self.importer.import_data(path)
+        return self.report[type].generate(self.data)
